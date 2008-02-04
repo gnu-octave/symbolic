5edda73ad9debfd0f9d2f17cf8851b2d0425f08f
adb014 <devnull@localhost>
2008-02-04 05:51:49 +0000

    Remove references to Octave in the GPL copyright strings

--- inst/poly2sym.m
+++ inst/poly2sym.m
@@ -1,20 +1,18 @@
 ## Copyright (C) 2003 Willem J. Atsma
 ##
-## This file is part of Octave.
-##
-## Octave is free software; you can redistribute it and/or
+## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public
 ## License as published by the Free Software Foundation;
 ## either version 2, or (at your option) any later version.
 ##
-## Octave is distributed in the hope that it will be useful,
+## This software is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied
 ## warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 ## PURPOSE.  See the GNU General Public License for more
 ## details.
 ##
 ## You should have received a copy of the GNU General Public
-## License along with Octave; see the file COPYING.  If not,
+## License along with this software; see the file COPYING.  If not,
 ## write to the Free Software Foundation, 51 Franklin Street, 
 ## Fifth Floor, Boston, MA  02110-1301  USA
 
