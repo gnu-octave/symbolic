57c785939bd90bdc51d98186591f4c65d980573f
Colin Macdonald <macdonald@maths.ox.ac.uk>
2014-02-26 00:29:01 +0000

    update functions to use sympy communication fcn

--- @sym/mtimes.m
+++ @sym/mtimes.m
@@ -1,25 +1,18 @@
 function z = mtimes(a, b)
+%*   Matrix multiplication of inputs
+%
+%   TODO: currently will fail if either input is a matrix
 
   a = sym(a);
   b = sym(b);
 
-  f = fopen('sym_python_temp.py', 'w');
-  fprintf(f, 'import sympy as sp\nimport pickle\n');
-
-  fprintf(f, 'a = pickle.loads("""%s""")\n', a.pickle);
-  fprintf(f, 'b = pickle.loads("""%s""")\n', b.pickle);
-  fprintf(f, 'z = a * b\n');
-  fprintf(f, 'print "__________"\n');
-  fprintf(f, 'print str(z)\n');
-  fprintf(f, 'print "__________"\n');
-  fprintf(f, 'print pickle.dumps(z)\n');
-  fclose(f);
-  [status,out] = system('python sym_python_temp.py');
-  if status ~= 0
-    error('failed');
-    out
+  if ~isscalar(a) || ~isscalar(b)
+    error('TODO: non-scalar multiplication not implemented yet');
   end
-  A = regexp(out, '__________\n(.*)\n__________\n(.*)', 'tokens');
-  s.text = A{1}{1};
-  s.pickle = A{1}{2};
-  z = class(s, 'sym');
+
+  cmd = [ 'def fcn(ins):\n'  ...
+          '    (a,b) = ins\n'  ...
+          '    return (a*b,)\n' ];
+
+  z = python_sympy_cmd(cmd, a, b);
+
--- @sym/plus.m
+++ @sym/plus.m
@@ -1,27 +1,13 @@
 function z = plus(a, b)
-%
-%
+%+   Plus
+%   X + Y adds syms X and Y.  If only one input is a sym, trys to
+%   coerce the other to a sym.
 
-  a = sym(a);
-  b = sym(b);
+  a = sym (a);
+  b = sym (b);
 
-  f = fopen('sym_python_temp.py', 'w');
-  fprintf(f, 'import sympy as sp\nimport pickle\n');
+  cmd = [ 'def fcn(ins):\n'  ...
+          '    (a,b) = ins\n'  ...
+          '    return (a+b,)\n' ];
 
-  fprintf(f, 'a = pickle.loads("""%s""")\n', a.pickle);
-  fprintf(f, 'b = pickle.loads("""%s""")\n', b.pickle);
-  fprintf(f, 'z = a + b\n');
-  fprintf(f, 'print "__________"\n');
-  fprintf(f, 'print str(z)\n');
-  fprintf(f, 'print "__________"\n');
-  fprintf(f, 'print pickle.dumps(z)\n');
-  fclose(f);
-  [status,out] = system('python sym_python_temp.py');
-  if status ~= 0
-    error('failed');
-    out
-  end
-  A = regexp(out, '__________\n(.*)\n__________\n(.*)', 'tokens');
-  s.text = A{1}{1};
-  s.pickle = A{1}{2};
-  z = class(s, 'sym');
+  z = python_sympy_cmd (cmd, a, b);
