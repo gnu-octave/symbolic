2aaf9b6d167a318c426b489e75327c8619093e2d
Colin Macdonald <macdonald@maths.ox.ac.uk>
2014-02-26 00:26:44 +0000

    functions to centralize the sympy calls

---
+++ @sym/python_sympy_cmd.m
@@ -0,0 +1,34 @@
+function varargout = python_sympy_cmd(cmd, varargin)
+%PYTHON_SYMPY_CMD  Run SympPy command and return symbolic expressions
+%   Run a Python SymPy command which takes some 'sym' objects as
+%   inputs and return 'sym' objects as outputs.
+%
+%   For example:
+%
+%      [a,b,c,...] = python_sympy_cmd(cmd, x, y, z, ...)
+%
+%   where x,y,z are of class 'sym'.  'cmd' is a string consisting
+%   of python code to define a python function called 'fcn'.  This
+%   function takes a list of inputs and return a tuple of outputs.
+%
+%      cmd = [ ...
+%          'def fcn(ins):\n'  ...
+%          '    (x,y) = ins\n'  ...
+%          '    return (x+y,x-y)\n'  ...
+%      ];
+%
+%
+
+  A = python_sympy_cmd_raw(cmd, varargin{:});
+
+  % TODO: constructing sym() outside of contructor feels crude,
+  % provide a way for constructor to take these inputs?
+
+  for i=1:nargout
+    s = [];
+    s.text = A{2*i-1};
+    s.pickle = A{2*i};
+    s = class(s, 'sym');
+    varargout{i} = s;
+  end
+
