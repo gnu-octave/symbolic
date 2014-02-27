436db6ed233f0a7dbbbc7abf8afc24c7ab7419d0
Colin Macdonald <macdonald@maths.ox.ac.uk>
2014-02-27 00:41:00 +0000

    quiet syms, add comments

--- syms.m
+++ syms.m
@@ -1,9 +1,18 @@
 function syms(varargin)
-  nargin
+%SYMS  Create symbolic variables
+%   This is a convenience function.  Type
+%      syms x y z
+%   instead of
+%      x = sym('x');
+%      y = sym('y');
+%      z = sym('z');
+%
+%   Careful: this code runs evalin(): you should not use it
+%   (programmatically) on strings you don't trust.
+
   for i = 1:nargin
     cmd = sprintf('%s = sym(''%s'');', varargin{i}, varargin{i});
-    disp(['evaluating command: ' cmd])
+    %disp(['evaluating command: ' cmd])
     evalin('caller', cmd)
   end
-  %varargin
 
