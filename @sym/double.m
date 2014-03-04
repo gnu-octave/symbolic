4cde83c0f8d41277f6d3a5dd78cb7048cb1dd527
Colin Macdonald <macdonald@maths.ox.ac.uk>
2014-03-04 15:27:09 +0000

    double fix on older octave

--- @sym/double.m
+++ @sym/double.m
@@ -1,4 +1,4 @@
-function y = double2(x)
+function y = double(x)
 %DOUBLE   Convert symbolic to doubles
 
   cmd = [ 'def fcn(ins):\n'  ...
@@ -6,8 +6,20 @@ function y = double2(x)
           '    y = sp.N(x,30)\n'  ...
           '    return (y,)\n' ];
   A = python_sympy_cmd_raw(cmd, x);
+
+  % workaround for Octave 3.6.4 where str2double borks on "inf"
+  % instead of "Inf".
+  % http://hg.savannah.gnu.org/hgweb/octave/rev/a08f6e17336e
+  %if (is_octave())
+  if exist('octave_config_info', 'builtin');
+    if (compare_versions (OCTAVE_VERSION (), '3.8.0', '<'))
+      A{1} = strrep(A{1}, 'inf', 'Inf');
+    end
+  end
+
   y = str2double (A{1});
   if (isnan (y))
+    A
     error('conversion failed?');
   end
 
