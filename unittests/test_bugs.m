09f126aaa8415a55e060db6ee8121623c832a601
Colin Macdonald <macdonald@maths.ox.ac.uk>
2014-04-04 12:46:09 +0800

    mark failures in test_bugs but don't run the codes for now

--- unittests/test_bugs.m
+++ unittests/test_bugs.m
@@ -4,30 +4,32 @@ function r = test_bugs()
 
   warning('todo known failures to fix')
 
+
   D = [0 1; 2 3];
 
   %% array construction: this works
   A = [sym(0) 1; sym(2) 3];
-  c=c+1; r(c) = true;
+  c=c+1; r(c) = all(all( A == D ));
 
   %% but this fails
-  try
-    A = [sym(0) 1; 2 3];
-    c=c+1; r(c) = true;
-  catch
-    c=c+1; r(c) = false;
-  end
+  %try
+  %  A = [sym(0) 1; 2 3];
+  %  c=c+1; r(c) = true;
+  %catch
+  %  c=c+1; r(c) = false;
+  %end
+  c=c+1; r(c) = false;
 
-  %% size issues
-  c=c+1; r(c) = all(  size(any(A-D)) == [1 2]  )
-  c=c+1; r(c) = all(  size(double(A)) == size(A)  )
+
+  %% size issues, any and double not implemented
+  %c=c+1; r(c) = all(  size(any(A-D)) == [1 2]  )
+  %c=c+1; r(c) = all(  size(double(A)) == size(A)  )
+  c=c+1; r(c) = false;
+  c=c+1; r(c) = false;
 
 
   DZ = D - D;
 
-
-  %% todo: need symzeros cmd
-  %c=c+1; r(c) = all(all(logical(  D - A == DZ  )));
 
 
   % todo: bug? boolean not converted to sym
@@ -36,4 +38,6 @@ function r = test_bugs()
   %sym(1==0)
   %x - (1==1)
   %x - (1==0)
+
+  %stop
 
