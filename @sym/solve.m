58cd4e2d91188d5a82dfb912fc627d184470f435
Colin Macdonald <macdonald@maths.ox.ac.uk>
2014-04-04 16:28:59 +0100

    solve: works with systems, output still WIP

--- @sym/solve.m
+++ @sym/solve.m
@@ -1,11 +1,21 @@
-function out = solve(eqn, vars)
+function out = solve(varargin)
 %SOLVE   Symbolic solutions of eqns and systems
 %   solve(x==2*x+6, x)
+%   solve(eq1, eq2, var1, var2)
+%   solve(eq1,...,eqn, var1,...,varn)
+%
+%   todo: omitting the vars should work too
+%   solve(eq1,...,eqn)
+%
 %   todo: out is a python list
 
-  cmd = [ 'def fcn(_ins):\n'  ...
-          '    sys.stderr.write("pydebug: " + str(_ins) + "\\n")\n'  ...
-          '    d = sp.solve(*_ins)\n'  ...
-          '    sys.stderr.write("pydebug: " + str(d) + "\\n")\n'  ...
+  n = nargin;
+  eqn = varargin(1:n/2)
+  vars = varargin(n/2+1:n)
+
+  cmd = [ 'def fcn(_ins):\n'                                          ...
+          '    sys.stderr.write("pydebug: " + str(_ins) + "\\n")\n'   ...
+          '    d = sp.solve(*_ins, dict=True)\n'                       ...
+          '    sys.stderr.write("pydebug: " + str(d) + "\\n")\n'      ...
           '    return (d,)\n' ];
   out = python_sympy_cmd (cmd, eqn, vars);
