c551108362dffd6e7d361244fe52e988638871e3
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-04-08 17:30:48 +0100

    fourier transf and inv

---
+++ @sym/ifourier.m
@@ -0,0 +1,14 @@
+function f = ifourier(F,k,x)
+%IFOURIER   Symbolic inverse Fourier transform
+
+  if (nargin < 3)
+    syms k
+    warning('todo: check SMT for 2nd argument behavoir');
+  end
+
+  cmd = [ 'def fcn(_ins):\n'                                      ...
+          '    d = sp.inverse_fourier_transform(*_ins)\n'            ...
+          '    return (d,)\n' ];
+
+  f = python_sympy_cmd (cmd, F, k, x);
+
---
+++ unittests/test_transforms.m
@@ -0,0 +1,10 @@
+function r = test_transforms()
+  c = 0; r = [];
+
+  syms x k
+  f = exp(-x^2);
+  F = fourier(f,x,k);
+  g = ifourier(F,k,x);
+  c=c+1; r(c) = isequal(f,g);
+
+  %todo laplace
