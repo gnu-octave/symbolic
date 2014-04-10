8b7d154340d496afbd033ecef73f87482753aebc
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-04-10 17:33:48 +0800

    symfun needs numel, not sure why
    otherwise, some kind of octave recursion, at least on 3.6.4
    
    for now, hardcode numel to 1

---
+++ @symfun/numel.m
@@ -0,0 +1,8 @@
+function n = numel(f)
+%NUMEL   Number of elements in symbolic array
+%   todo: why do i need this? why is it called so much?
+
+  disp('symfun numel call, prob wrong')
+  %f
+  n = 1;
+
