bcaef70e2a292cfa8df4fd9b43e6ee8dc533fd21
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-04-11 11:48:23 +0800

    tests updated for bugs

--- unittests/test_bugs.m
+++ unittests/test_bugs.m
@@ -2,52 +2,43 @@ function r = test_bugs()
   c = 0; r = [];
   syms x
 
-  % Issue #5, scalar expansion
-  a = sym(1);
-  a(2) = 2;
-  c=c+1; r(c) = isequal(a, [1 2]);
 
 
+  %% Issue #8
+  % array construction when row is only doubles
+  % fails on: Octave 3.6.4 *and* 3.8.1
+  % works on: Matlab
+  try
+    %A = [sym(0) 1; sym(2) 3];  % this works
+    A = [sym(0) 1; 2 3];
+    c=c+1; r(c) = isequal(A, [1 2; 3 4]);
+  catch
+    c=c+1; r(c) = false;
+  end
 
-  warning('todo known failures to fix')
 
-
+  %% size issues(?), "any()" not implemented
   D = [0 1; 2 3];
-
-  %% array construction: this works
-  A = [sym(0) 1; sym(2) 3];
-  c=c+1; r(c) = all(all( A == D ));
-
-  %% but this fails
-  %try
-  %  A = [sym(0) 1; 2 3];
-  %  c=c+1; r(c) = true;
-  %catch
-  %  c=c+1; r(c) = false;
-  %end
-  c=c+1; r(c) = false;
+  A = sym(D);
+  c=c+1; r(c) = isequal( size(any(A-D)), [1 2] );
 
 
-  %% size issues, any and double not implemented
-  %c=c+1; r(c) = all(  size(any(A-D)) == [1 2]  )
-  %c=c+1; r(c) = all(  size(double(A)) == size(A)  )
-  c=c+1; r(c) = false;
-  c=c+1; r(c) = false;
-
-
-  DZ = D - D;
-
-
-
-  % todo: bug? boolean not converted to sym
+  %% todo: bug? boolean not converted to sym
   c=c+1; r(c) = 0;
   %sym(1==1)
   %sym(1==0)
   %x - (1==1)
   %x - (1==0)
 
-  % bug!  inf/nan in array ctor makes wrong matrix
-  a = sym([nan 1 2]);
-  c=c+1;  r(c) = isequaln(  a, [nan 1 2]  );
-  a = sym([1 inf]);
-  c=c+1;  r(c) = isequaln(  a, [1 inf]  );
+
+
+  %% Issue #9
+  % nan == 1 should not be the sym "nan == 1" but rather bool false
+  snan = sym(0)/0;
+  c=c+1; r(c) = islogical(snan == 1);
+  c=c+1; r(c) = ~isa(snan == 1, 'sym');
+  c=c+1; r(c) = islogical([snan snan 1] == [10 12 1]);
+
+  % these seem to work
+  c=c+1; r(c) = islogical(sym(inf) == 1);
+  c=c+1; r(c) = (sym(inf) == 1) == false;
