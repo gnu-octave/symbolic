469f9c8a06230045b67bcd3b787cfe1e4f7391b5
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-04-11 20:58:08 +0800

    somehow never added length.m

---
+++ @sym/length.m
@@ -0,0 +1,5 @@
+function n = length(x)
+%LENGTH  Length of a symbolic array
+
+  d = size(x);
+  n = max(d);
---
+++ unittests/test_array_sizes.m
@@ -0,0 +1,18 @@
+function r = test_array_sizes()
+  c = 0; r = [];
+
+  a = sym([1 2 3]);
+  c=c+1; r(c) = length(a) == 3;
+  c=c+1; r(c) = numel(a) == 3;
+  c=c+1; r(c) = isequal(size(a), [1 3]);
+  c=c+1; r(c) = isa(size(a), 'double');
+  c=c+1; r(c) = isa(numel(a), 'double');
+  c=c+1; r(c) = isa(length(a), 'double');
+
+  a = sym([1 2 3; 4 5 6]);
+  c=c+1; r(c) = length(a) == 3;
+  c=c+1; r(c) = numel(a) == 6;
+  c=c+1; r(c) = isequal(size(a), [2 3]);
+  c=c+1; r(c) = isa(size(a), 'double');
+  c=c+1; r(c) = isa(numel(a), 'double');
+  c=c+1; r(c) = isa(length(a), 'double');
