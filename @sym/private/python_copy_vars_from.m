0cd10a243a21e6cc837188152fd958f3439b2e57
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-04-14 17:23:13 +0100

    unicode pretty working

--- @sym/private/python_copy_vars_from.m
+++ @sym/private/python_copy_vars_from.m
@@ -12,11 +12,12 @@ function s = python_copy_vars_from(out)
   s = sprintf('%sprint "%s"\n', s, etag);
   s = sprintf('%sfor item in %s:\n', s, out);
   s = sprintf('%s    print "%s"\n', s, stag);
-  s = sprintf('%s    print str(item)\n', s);
+  s = sprintf('%s    print str("fixme placeholder")\n', s);
   s = sprintf('%s    print "%s"\n', s, etag);
   s = sprintf('%s    print "%s"\n', s, stag);
   s = sprintf('%s    #print pickle.dumps(item, 0)\n', s);
   s = sprintf('%s    #print sp.srepr(item)\n', s);
+  s = sprintf('%s    #dbout(("before octcmd", item))\n', s);
   s = sprintf('%s    print "tmp = " + octcmd(item) + ";"\n', s);
   s = sprintf('%s    print "%s"\n\n', s, etag);
   s = sprintf('%sprint "%s"\n\n', s, ebtag);
