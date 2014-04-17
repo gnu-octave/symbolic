af7ba56a045f8e19abd439ebba5337bedda95cb5
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-04-17 16:43:03 +0800

    command to reset the ipc

---
+++ inst/octsympy_config.m
@@ -0,0 +1,36 @@
+%% Copyright (C) 2014 Colin B. Macdonald
+%%
+%% This file is part of Octave-Symbolic-SymPy
+%%
+%% Octave-Symbolic-SymPy is free software; you can redistribute
+%% it and/or modify it under the terms of the GNU General Public
+%% License as published by the Free Software Foundation;
+%% either version 3 of the License, or (at your option) any
+%% later version.
+%%
+%% This software is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty
+%% of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+%% the GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public
+%% License along with this software; see the file COPYING.
+%% If not, see <http://www.gnu.org/licenses/>.
+
+%% -*- texinfo -*-
+%% @deftypefn {Function File} {@var{r} =} octsympy_config ()
+%% Configure the SymPy communication mechanism.
+%%
+%% FIXME: return values?
+%%
+%% @seealso{syms, octsympy_config}
+%% @end deftypefn
+
+function varargout = octsympy_config()
+
+
+end
+
+
+%!test
+%! assert(true)
