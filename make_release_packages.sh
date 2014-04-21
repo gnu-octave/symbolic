6cd78e12fc78c72e51d55344e639e689d9e7f950
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-04-21 15:51:14 +0800

    a script to make packages for website release

---
+++ make_release_packages.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+VER=0.0.2
+PKG=octsympy-$VER
+DIR=$PKG
+
+echo "Making packages for octsympy-$VER."
+
+read -p "Press [Enter] to git clone and make packages..."
+
+
+# checkout a clean copy
+rm -rf octsympy
+git clone https://github.com/cbm755/octsympy.git
+pushd octsympy
+git checkout tags/v0.0.2
+popd
+
+
+# clean up
+rm -f ${PKG}.tar.gz ${PKG}.zip
+rm -rf ${DIR}
+cp -r octsympy ${DIR}
+
+# remove .git dir and other things not needed for package
+pushd ${DIR}/
+rm -rf .git/
+rm -f screenshot.png
+popd
+
+# make clean
+pushd ${DIR}/src/
+make clean
+popd
+
+# here are the packages
+tar -zcvf ${PKG}.tar.gz ${DIR}
+zip -r ${PKG}.zip ${DIR}
+
+md5sum ${PKG}.tar.gz ${PKG}.zip
