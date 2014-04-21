18da60e09e86a678b2ca44914f5801b72c4b1208
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-04-21 15:56:25 +0800

    run_octave_tests should work after installing package
    perhaps some tests won't work this way (any in private?) and my older unittest framework is still not supported but its a start

--- inst/run_octave_tests.m
+++ inst/run_octave_tests.m
@@ -1,5 +1,5 @@
+methods_list = methods(sym);
 base = '@sym';
-files = dir(base);
 
 % usually I'd just use cputime(), but some of our IPC mechanisms are
 % light on CPU and heavy on IO.
@@ -8,21 +8,18 @@ totalcputime = cputime();
 
 num_tests = 0;
 num_passed = 0;
-for i=1:length(files)
-  mfile = files(i).name;
-  if ( (~files(i).isdir) && strcmp(mfile(end-1:end), '.m') )
-    str = mfile(1:end-2);
-    fprintf('**** Looking for tests in: %s\n', mfile);
-    [N,MAX] = test([base '/' str]); %, 'quiet');
-    num_tests = num_tests + MAX;
-    num_passed = num_passed + N;
-    % this doesn't really work, bug in Octave?  MAX,N always zero
-    % if failures.  https://savannah.gnu.org/bugs/?42150
-    if (MAX > 0)
-      fprintf('**** %s Passed %d of %d\n', mfile, N, MAX);
-      fprintf('%s\n\n', char('_'*ones(1,80)));
-      %fprintf('  (paused)\n'); pause
-    end
+for i=1:length(methods_list)
+  m = methods_list{i};
+  fprintf('**** Looking for tests in: %s\n', m);
+  [N,MAX] = test([base '/' m]); %, 'quiet');
+  num_tests = num_tests + MAX;
+  num_passed = num_passed + N;
+  % this doesn't really work, bug in Octave?  MAX,N always zero
+  % if failures.  https://savannah.gnu.org/bugs/?42150
+  if (MAX > 0)
+    fprintf('**** %s Passed %d of %d\n', m, N, MAX);
+    fprintf('%s\n\n', char('_'*ones(1,80)));
+    %fprintf('  (paused)\n'); pause
   end
 end
 
