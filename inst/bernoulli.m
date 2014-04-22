c209f95ac7f3c13846755c0cd7ab96d69dade0be
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-04-22 17:16:11 +0800

    bernouilli
    note, not in @sym b/c it can be called with a double

---
+++ inst/bernoulli.m
@@ -0,0 +1,39 @@
+%% Copyright (C) 2014 Colin B. Macdonald
+%%
+%% This file is part of OctSymPy
+%%
+%% OctSymPy is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published
+%% by the Free Software Foundation; either version 3 of the License,
+%% or (at your option) any later version.
+%%
+%% This software is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty
+%% of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+%% the GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public
+%% License along with this software; see the file COPYING.
+%% If not, see <http://www.gnu.org/licenses/>.
+
+%% -*- texinfo -*-
+%% @deftypefn  {Function File} {@var{b} =} bernoulli (@var{n})
+%% @deftypefnx {Function File} {@var{p} =} bernoulli (@var{n}, @var{x})
+%% Return Bernouilli numbers and polynomials
+%%
+%%
+%% @seealso{euler}
+%% @end deftypefn
+
+%% Author: Colin B. Macdonald
+%% Keywords: symbolic
+
+function r = bernoulli(n, x)
+
+  if (nargin == 1)
+    r = python_cmd ('return sp.bernoulli(*_ins),', n);
+  else
+    r = python_cmd ('return sp.bernoulli(*_ins),', n, sym(x));
+  end
+
+end
