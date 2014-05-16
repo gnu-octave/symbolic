8f435f3f931b3a7e31dd3e32767fdc3234df45c6
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-05-16 21:08:16 +0100

    subsindex, colon

--- inst/@sym/colon.m
+++ inst/@sym/colon.m
@@ -17,46 +17,59 @@
 %% If not, see <http://www.gnu.org/licenses/>.
 
 %% -*- texinfo -*-
-%% @deftypefn  {Function File} {@var{i}} subsindex (@var{x})
-%% Used to implement indexing by sym.
+%% @deftypefn  {Function File} {@var{x}} colon (@var{a},@var{b})
+%% @deftypefnx {Function File} {@var{x}} colon (@var{a},@var{step},@var{b})
+%% Generate a range of syms.
+%%
+%% FIXME: should this generate intervals?  Check what SMT does?
 %%
 %% @end deftypefn
 
 %% Author: Colin B. Macdonald
 %% Keywords: symbolic
 
-function b = subsindex(x)
+function y = colon(a,step,b)
 
-  % zero-based indexing
-  b = double(x) - 1;
+  if (nargin == 2)
+    b = step;
+    step = 1;
+  end
 
+  % make sure inputs convert without error
+  tilde = double(a);
+  tilde = double(b);
+  tilde = double(step);
+
+  % much faster (for ipc) in python
+  %y = sym( double(a):double(step):double(b) );
+  evalpy('y = range(a,b+sign(step)*1,step);;', a, b, step);
+  % FIXME: if evalpy learned arrays, could drop this...
+  y = cell2mat(y);
 end
 
+%!test
+%! a = sym(1):5;
+%! b = sym(1:5);
+%! assert(isequal(a,b));
+%! a = 1:sym(5);
+%! b = sym(1:5);
+%! assert(isequal(a,b));
 
 %!test
-%! i = sym(1);
-%! a = 7;
-%! assert(a(i)==a);
-%! i = sym(2);
-%! a = 2:2:10;
-%! assert(a(i)==4);
+%! a = 2:sym(2):8;
+%! b = sym(2:2:8);
+%! assert(isequal(a,b));
 
 %!test
-%! i = sym([1 3 5]);
-%! a = 1:10;
-%! assert(a(i)==[1 3 5]);
-
-%!test
-%! i = sym([1 3 5]);
-%! a = sym(1:10);
-%! assert(isequal(a(i),sym([1 3 5])));
+%! a = sym(10):-2:-4;
+%! b = sym(10:-2:-4);
+%! assert(isequal(a,b));
 
 %!test
 %! % should be an error if it doesn't convert to double
 %! syms x
-%! a = 1:10;
 %! try
-%!   a(x)
+%!   a = 1:x;
 %!   waserr = false;
 %! catch
 %!   waserr = true;
