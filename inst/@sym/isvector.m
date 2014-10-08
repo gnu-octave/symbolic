3580efb7676da03f0e70b62d19430fa2da80e63f
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-10-08 21:25:05 +0100

    add isvector, octave dev version now needs this

--- inst/@sym/isvector.m
+++ inst/@sym/isvector.m
@@ -17,28 +17,26 @@
 %% If not, see <http://www.gnu.org/licenses/>.
 
 %% -*- texinfo -*-
-%% @deftypefn  {Function File} {@var{b} =} isscalar (@var{x})
-%% Return true if this symbolic expression is a scalar.
+%% @deftypefn  {Function File} {@var{b} =} isvector (@var{x})
+%% Return true if this symbolic expression is a vector.
 %%
-%% @seealso{size, numel, isvector}
+%% @seealso{size, numel, isscalar}
 %% @end deftypefn
 
 %% Author: Colin B. Macdonald
 %% Keywords: symbolic
 
-function b = isscalar(x)
+function b = isvector(x)
 
   d = size(x);
-  n = prod(d);
-  b = (n == 1);
+  b = any(d == 1);
 
 end
 
 
-%!assert(isscalar(sym('x')))
-
-%!test
-%! a = sym([1 2 3]);
-%! assert(~isscalar(a))
-
-%!assert(~isscalar(sym([])))
+%!assert(isvector(sym('x')))
+%!assert(isvector(sym([1 2 3])))
+%!assert(isvector(sym([1; 2])))
+%!assert(isvector(sym([1; 2])))
+%!assert(~isvector(sym([1 2; 3 4])))
+%!assert(~isvector(sym([])))
