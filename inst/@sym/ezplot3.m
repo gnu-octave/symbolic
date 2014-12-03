8d693348c2e3cd0168507eee4d277086d92b0c49
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-12-03 23:59:28 +0000

    ezplot3: sanity checks

--- inst/@sym/ezplot3.m
+++ inst/@sym/ezplot3.m
@@ -41,12 +41,33 @@ function varargout = ezplot3(varargin)
   end
 
   maxnumsym = 3;
+  firstsym = [];
 
   for i = firstpotsym:nargin
     if (isa(varargin{i}, 'sym'))
       if (i < firstpotsym + maxnumsym)
         % one of the fcns to plot, covert to handle fcn
-        varargin{i} = matlabFunction(varargin{i});
+
+        % Each is function of one var, and its the same var for all
+        thissym = symvar(varargin{i});
+        assert(length(thissym) <= 1, ...
+          'ezplot3: plotting curves: functions should have at most one input');
+        if (isempty(thissym))
+          % a number, create a constant function in a dummy variable
+          % (0*t works around some Octave oddity on 3.8 and hg Dec 2014)
+          varargin{i} = inline(sprintf('%g + 0*t', double(varargin{i})), 't');
+          % varargin{i} = @(t) 0*t + double(varargin{i});  % no
+        else
+          % check variables match (sanity check)
+          if (isempty(firstsym))
+            firstsym = thissym;
+          else
+            assert(logical(thissym == firstsym), ...
+              'ezplot3: all functions must be in terms of the same variables');
+          end
+          varargin{i} = matlabFunction(varargin{i});
+        end
+
       else
         % plot ranges, etc, convert syms to doubles
         varargin{i} = double(varargin{i});
@@ -74,6 +95,14 @@ end
 %! warning(s)
 %! zz = get(h, 'zdata');
 %! assert (abs(zz(end) - 2*pi) <= 4*eps)
+
+%!error <all functions must be in terms of the same variables>
+%! syms x t
+%! ezplot3(t, x, t)
+
+%!error <functions should have at most one input>
+%! syms x t
+%! ezplot3(t, t*x, t)
 
 %%!test
 %%! % bounds etc as syms
