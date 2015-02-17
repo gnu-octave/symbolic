1442dabff0cf8ea1158aaea742348b046425c012
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2015-02-17 22:27:32 +0000

    development version numbers follow PEP 440

--- inst/sympref.m
+++ inst/sympref.m
@@ -130,7 +130,7 @@ function varargout = sympref(cmd, arg)
 
     case 'version'
       assert (nargin == 1)
-      varargout{1} = '2.0.0-git';
+      varargout{1} = '2.1.0.dev';
 
     case 'display'
       if (nargin == 1)
--- make_windows_package.sh
+++ make_windows_package.sh
@@ -8,9 +8,9 @@ PYEXEREADME=py27910.readme.txt   # from the src package
 SYMPY=sympy-0.7.6
 
 # for day-to-day testing
-VER=2.0.0-git
+VER=2.1.0.dev
 # for release
-#VER=2.0.0
+#VER=2.1.0
 #TAG=v${VER}
 
 
