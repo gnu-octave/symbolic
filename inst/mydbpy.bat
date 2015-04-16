6af51fc37a816bb4cc63cc5e143915631637ecd2
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2015-04-16 13:52:42 +0100

    windows bat file hack: improve doc

--- inst/mydbpy.bat
+++ inst/mydbpy.bat
@@ -1,13 +1,15 @@
 @echo OFF
 
-REM Octave for windows cannot tolerate stderr from a popen2 subprocess
-REM This bat file discards stderr from py.exe
+REM This bat file discards stderr from python.exe.
+REM This is needed as Octave for Windows cannot tolerate stderr from a popen2
+REM subprocess: see [this bug report](https://savannah.gnu.org/bugs/?43036).
+
 REM We look for python.exe and use that if found.  Otherwise we try py.exe.
 REM You'll need to edit this script if your python is called something else.
 
 REM This batch file uses the "where" command, which is present only for
 REM Windows Server 2003 and later versions. So, for older windows systems (such
-REM as Windows XP) only lines 15 or 18 should be kept in the next code block.
+REM as Windows XP) only lines 17 or 20 should be kept in the next code block.
 
 REM returns 0 if python.exe is found
 where /Q python.exe
@@ -19,9 +21,10 @@ py.exe -i 2> NUL
 :endif
 
 REM If python is not installed in your system, "py.exe" will be used.
-REM Please, include its folder (for instance: 
-REM    C:\Octave\Octave-3.8.2\\share\octave\package\octosympy-0.0.1\bin     )
-REM in the windows environment variable %PATH 
+REM It is [available online](http://www.orbitals.com/programs/pyexe.html)
+REM You may need to include its folder (e.g.,
+REM    C:\Octave\Octave-3.8.2\share\octave\package\octsympy-2.1.1\bin)
+REM in the windows environment variable %PATH
 
 REM Other options for the stderr:
 
