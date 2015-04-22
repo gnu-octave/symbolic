37c559519582e7fece28c3d92eb3293d6dcdfa05
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2015-04-22 14:05:16 +0100

    maint: post release version numbers

--- inst/sympref.m
+++ inst/sympref.m
@@ -139,7 +139,7 @@
 %% @example
 %% @group
 %% >> sympref version
-%%    @result{} 2.2.1
+%%    @result{} 2.2.2-dev
 %% @end group
 %% @end example
 %%
@@ -172,7 +172,7 @@ function varargout = sympref(cmd, arg)
 
     case 'version'
       assert (nargin == 1)
-      varargout{1} = '2.2.1';
+      varargout{1} = '2.2.2-dev';
 
     case 'display'
       if (nargin == 1)
