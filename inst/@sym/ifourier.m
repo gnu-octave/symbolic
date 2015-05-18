ca8468027badc6f73152738bd73cbe122e182948
Andrés Prieto <prieto.aneiros@gmail.com>
2015-05-18 17:59:45 +0200

    Update (i)fourier and (i)laplace functions

--- inst/@sym/ifourier.m
+++ inst/@sym/ifourier.m
@@ -1,4 +1,4 @@
-%% Copyright (C) 2014 Colin B. Macdonald
+%% Copyright (C) 2014, 2015 Andrés Prieto, Colin B. Macdonald
 %%
 %% This file is part of OctSymPy.
 %%
@@ -17,31 +17,94 @@
 %% If not, see <http://www.gnu.org/licenses/>.
 
 %% -*- texinfo -*-
-%% @deftypefn  {Function File} {@var{f} =} ifourier (@var{FF}, @var{k}, @var{x})
-%% Symbolic inverse Fourier transform.
+%% @documentencoding UTF-8
+%% @deftypefn {Function File} {@var{f} =} ifourier (@var{FF}, @var{k}, @var{x})
+%% @deftypefnx {Function File} {@var{f} =} ifourier (@var{FF})
+%% @deftypefnx {Function File} {@var{f} =} ifourier (@var{FF}, @var{x})
+%% Symbolic inverse Fourier transform (non-unitary, angular frequency).
 %%
-%% FIXME: doc, examples
+%% Example:
+%% @example
+%% @group
+%% >> syms k
+%% >> F = 1/k^2;
+%% >> ifourier(F)
+%%    @result{} (sym)
+%%        2
+%%        ──
+%%         3
+%%        s
+%% @end group
+%% @end example
 %%
-%% @seealso{ilaplace}
+%% @seealso{fourier}
 %% @end deftypefn
 
-%% Author: Colin B. Macdonald
-%% Keywords: symbolic
+%% Author: Colin B. Macdonald, Andrés Prieto
+%% Keywords: symbolic, integral transforms
 
-function f = ifourier(F, k, x)
+function f = ifourier(varargin)
 
-  if (nargin < 3)
-    syms k
-    warning('todo: check SMT for 2nd argument behavoir');
+  % FIXME: it only works for scalar functions
+  % FIXME: it doesn't handle diff call (see SMT transform of diff calls)
+
+  % If the physical variable of f is equal to "s",
+  % "t" is the frequency domain variable (analogously to SMT)
+  if (nargin == 1)
+    F=varargin{1};
+    k=symvar(F,1);
+    cmd = { 'F=_ins[0]; k=_ins[1]; x=sp.Symbol("x")'
+            'if x==k:'
+            '    x=sp.Symbol("t")'
+            'f = sp.inverse_fourier_transform(F, k, x/(2*sp.pi))/(2*sp.pi)'
+            'return f,'};
+
+    f = python_cmd(cmd, F, k);
+
+  elseif (nargin == 2)
+    F=varargin{1};
+    x=varargin{2}; 
+    k=symvar(F,1);
+    cmd = { 'F=_ins[0]; k=_ins[1]; x=_ins[2]'
+            'f = sp.inverse_fourier_transform(F, k, x/(2*sp.pi))/(2*sp.pi)'
+            'return f,'};
+
+    f = python_cmd(cmd, F, k, x);
+
+  elseif (nargin == 3)
+    F=varargin{1};
+    k=varargin{2}; 
+    x=varargin{3};
+    cmd = { 'F=_ins[0]; k=_ins[1]; x=_ins[2]'
+            'f = sp.inverse_fourier_transform(F, k, x/(2*sp.pi))/(2*sp.pi)'
+            'return f,'};
+
+    f = python_cmd(cmd, F, k, x);
+
+  else
+    print_usage ();
+
   end
-
-  cmd = { 'd = sp.inverse_fourier_transform(*_ins)'
-          'return d,' };
-
-  f = python_cmd (cmd, sym(F), sym(k), sym(x));
 
 end
 
 
-%! % tests in fourier.m
-%!assert(true)
+%!test
+%! % basic
+%! syms t r u x w
+%! Pi=sym('pi');
+%! assert(logical( ifourier(exp(-abs(w))) == 1/(Pi*(x^2 + 1)) ))
+%! assert(logical( ifourier(exp(-abs(x))) == 1/(Pi*(t^2 + 1)) ))
+%! assert(logical( ifourier(exp(-abs(r)),u) == 1/(Pi*(u^2 + 1)) ))
+%! assert(logical( ifourier(exp(-abs(r)),r,u) == 1/(Pi*(u^2 + 1)) ))
+%! assert(logical( ifourier(exp(-w^2/4)) == 1/(sqrt(Pi)*exp(x^2)) ))
+%! assert(logical( ifourier(sqrt(Pi)/exp(w^2/4)) == exp(-x^2) ))
+
+%!xtest
+%! % Inverse Fourier transform cannot handle non-smooth functions
+%! % SymPy cannot evaluate correctly??
+%! syms x w
+%! assert(logical( ifourier(2/(w^2 + 1)) == exp(-abs(x)) ))
+%! assert(logical( ifourier(2/(w^2 + 1)) == heaviside(x)/exp(x) + heaviside(-x)*exp(x) ))
+%! assert(logical( ifourier(-(w*4)/(w^4 + 2*w^2 + 1) )== -x*exp(-abs(x))*1i ))
+%! assert(logical( ifourier(-(w*4)/(w^4 + 2*w^2 + 1) )== -x*(heaviside(x)/exp(x) + heaviside(-x)*exp(x))*1i ))
