49b1d1079cf40138a99eea4836cb3e4f30dcb8b8
Colin B. Macdonald <cbm@m.fsf.org>
2015-12-14 12:22:45 -0800

    [maint] post release version bump

--- make_windows_package.sh
+++ make_windows_package.sh
@@ -8,9 +8,9 @@ PYEXEREADME=py27910.readme.txt   # from the src package
 SYMPY=sympy-0.7.6.1
 
 # for day-to-day testing
-VER=2.2.4-dev
+VER=2.3.0-dev
 # for release
-#VER=2.2.4
+#VER=2.3.0
 #TAG=v${VER}
 
 
