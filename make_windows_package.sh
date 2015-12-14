1da9687b14a532f7f5729f769e185ce39e6583de
Colin B. Macdonald <cbm@m.fsf.org>
2015-12-14 11:32:15 -0800

    maint: use sympy 0.7.6.1 in windows package

--- make_windows_package.sh
+++ make_windows_package.sh
@@ -5,7 +5,7 @@ PYEXE=py27910.exe
 PYEXEREADME=py27910.readme.txt   # from the src package
 
 # download sympy release, unpack in the directory with this script
-SYMPY=sympy-0.7.6
+SYMPY=sympy-0.7.6.1
 
 # for day-to-day testing
 VER=2.2.4-dev
