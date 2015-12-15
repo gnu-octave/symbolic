ae8a15cd28792d9fce842d1c859b0e91ae08c79b
Colin B. Macdonald <cbm@m.fsf.org>
2015-12-14 16:06:54 -0800

    Add "sympref quiet on" option to partially suppress messages
    Not yet possible (I think) to make Python shutup during startup
    but this should minimize the messages we output!
    
    Fixes #273.

--- inst/private/python_ipc_system.m
+++ inst/private/python_ipc_system.m
@@ -14,9 +14,10 @@ function [A, out] = python_ipc_system(what, cmd, mktmpfile, varargin)
     error('unsupported command')
   end
 
-  vstr = sympref('version');
+  verbose = ~sympref('quiet');
 
-  if (isempty(show_msg))
+  if (verbose && isempty(show_msg))
+    vstr = sympref('version');
     disp(['OctSymPy v' vstr ': this is free software without warranty, see source.'])
     disp('You are using the slower system() communications with SymPy.')
     disp('Warning: this will be *SLOW*.  Every round-trip involves executing a')
