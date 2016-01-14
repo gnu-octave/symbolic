d0016b417251397956fdbc567ccf9509209567f5
Colin B. Macdonald <cbm@m.fsf.org>
2016-01-14 10:32:40 -0800

    reword comment to refer to SymPy > 0.7.6.1

--- inst/@sym/mpower.m
+++ inst/@sym/mpower.m
@@ -46,9 +46,8 @@ function z = mpower(x, y)
 
     z = python_cmd (cmd, sym(x), sym(y));
 
-  % Dear hacker, wait from next release of sympy (actually .7.6.1) and replace with this function:
-  % z = python_cmd ('return _ins[0]**_ins[1],', sym(x), sym(y))
-
+  % FIXME: with some future SymPy (>0.7.6.1?), we may be able to use:
+  %z = python_cmd ('return _ins[0]**_ins[1],', sym(x), sym(y))
 
 end
 
