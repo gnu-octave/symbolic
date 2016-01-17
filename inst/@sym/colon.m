f18113d01b7468292aae7e3f0f12bbc5aa9345f0
Colin B. Macdonald <cbm@m.fsf.org>
2016-01-16 22:10:52 -0800

    slightly clearer code, fix expected error, add docs
    Somewhat surprised that `x:x+5` works: add exam of that to docs.

--- inst/@sym/colon.m
+++ inst/@sym/colon.m
@@ -27,9 +27,25 @@
 %% @group
 %% sym(5):10
 %%   @result{} ans = (sym) [5  6  7  8  9  10]  (1×6 matrix)
+%% 0:sym(pi):5*sym(pi)
+%%   @result{} ans = (sym) [0  π  2⋅π  3⋅π  4⋅π  5⋅π]  (1×6 matrix)
+%% x:2:(x+6)
+%%   @result{} ans = (sym) [x  x + 2  x + 4  x + 6]  (1×4 matrix)
+%% @end group
+%% @end example
+%%
+%% The end point @var{b} might not be included:
+%% @example
+%% @group
 %% 0:sym(pi):10
 %%   @result{} ans = (sym) [0  π  2⋅π  3⋅π]  (1×4 matrix)
-%% 6:-3:-sym(pi)
+%% @end group
+%% @end example
+%%
+%% The @var{step} can be negative:
+%% @example
+%% @group
+%% sym(6):-3:-3
 %%   @result{} ans = (sym) [6  3  0  -3]  (1×4 matrix)
 %% @end group
 %% @end example
@@ -48,9 +64,9 @@ function y = colon(a, step, b)
   end
 
   cmd = { '(a, b, step) = _ins'
-          'B = int((b-a)/step)'
-          'y = step*Matrix([range(0, B+1)])'
-          'y = y.applyfunc(lambda c: c + a)'
+          'B = floor((b-a)/step)'
+          'y = Matrix([range(0, B+1)])'
+          'y = y*step + Matrix([[a]*y.cols])'
           'return y,' };
 
   y = python_cmd (cmd, sym(a), sym(b), sym(step));
@@ -93,5 +109,5 @@ end
 %! t = sym(1)/10;
 %! assert(isequal(L, [t p/3+t 2*p/3+t]));
 
-%! % should be an error if it doesn't convert to double
-%!error <can't convert> syms x; a = 1:x;
+%!error syms x; a = 0:x;
+%!error syms x; a = 1:x;
