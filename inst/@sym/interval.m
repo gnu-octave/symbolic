3d560cf3158e89c72b620f1c76710b851ad65059
latot <felipematas@yahoo.com>
2016-01-19 23:10:29 -0300

    remove interval(1)

--- inst/@sym/interval.m
+++ inst/@sym/interval.m
@@ -53,21 +53,13 @@
 
 function I = interval(varargin)
 
-  if (nargin < 1 || nargin > 4)
+  if (nargin < 2 || nargin > 4)
     print_usage();
   end
 
   varargin = sym(varargin);
 
-  if nargin == 1
-
-    I = python_cmd ('return Interval(*_ins),', varargin{1}, varargin{1}, varargin{2:nargin});
-
-  else
-
-    I = python_cmd ('return Interval(*_ins),', varargin{:});
-
-  end
+  I = python_cmd ('return Interval(*_ins),', varargin{:});
 
 end
 
