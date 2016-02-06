460cae33a54907aed368cb4234cc48bcee064f91
latot <felipematas@yahoo.com>
2016-02-06 13:17:50 -0300

    minor fix
    minor fix
    
    other

--- inst/python_cmd.m
+++ inst/python_cmd.m
@@ -141,14 +141,14 @@ function varargout = python_cmd(cmd, varargin)
 
     ex_popen2 = exist('popen2') > 1;
     ipc_s = sympref('ipc'); 
-    lerror = line_error_python('get');
 
     if strcmp(ipc_s, 'popen2') || strcmp(ipc_s, 'sysoneline') || (strcmp(ipc_s, 'default') && ex_popen2)
-      errlineno = A{3} - lerror;
+      errlineno = A{3} - line_error_python('get');
     elseif strcmp(ipc_s, 'system') || strcmp(ipc_s, 'systmpfile') || (strcmp(ipc_s, 'default') && !ex_popen2)
-      errlineno = A{3} - lerror - (nargin - 1)*2;
+      errlineno = A{3} - line_error_python('get') - (nargin - 1)*2;
     else
-      error("IPC system not supported yet.");
+      warning('IPC system not supported yet to handle the line error.');
+      error(A{2});
     end
 
     error(strcat(A{2}, ' (line: ', mat2str(errlineno), ')'));
