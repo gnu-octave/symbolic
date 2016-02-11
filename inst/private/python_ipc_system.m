066d676a82a00237c0a5e283f27e90c7ffa2afa1
Colin B. Macdonald <cbm@m.fsf.org>
2016-02-11 11:06:39 -0800

    add copyright header, texinfo docs

--- inst/private/python_ipc_system.m
+++ inst/private/python_ipc_system.m
@@ -1,10 +1,37 @@
+%% Copyright (C) 2014--2016 Colin B. Macdonald
+%%
+%% This file is part of OctSymPy.
+%%
+%% OctSymPy is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published
+%% by the Free Software Foundation; either version 3 of the License,
+%% or (at your option) any later version.
+%%
+%% This software is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty
+%% of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+%% the GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public
+%% License along with this software; see the file COPYING.
+%% If not, see <http://www.gnu.org/licenses/>.
+
+%% -*- texinfo -*-
+%% @deftypefn  {Function File}  {[@var{A}, @var{info}] =} python_ipc_system (@dots)
+%% Private helper function for Python IPC.
+%%
+%% @var{A} is the resulting object, which might be an error code.
+%%
+%% @var{info} usually contains diagnostics to help with debugging
+%% or error reporting.
+%%
+%% @code{@var{info}.prelines}: the number of lines of header code
+%% before the command starts.
+%%
+%% @code{@var{info}.raw}: the raw output, for debugging.
+%% @end deftypefn
+
 function [A, info] = python_ipc_system(what, cmd, mktmpfile, varargin)
-% Outputs:
-%   A: the resulting object.
-%   info: diagnostics to help with debugging or error reporting.
-%   info.prelines: the number of lines of header code before cmds.
-%   info.raw: the raw output.
-%   info can also be empty.
 
   persistent show_msg
 
