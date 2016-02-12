b73f9fa5abb9257d3f0edc1e22ba7de1de1609c9
Colin B. Macdonald <cbm@m.fsf.org>
2016-02-12 01:01:14 -0800

    trust the ipc knows out many prelines

--- inst/python_cmd.m
+++ inst/python_cmd.m
@@ -138,19 +138,7 @@ function varargout = python_cmd(cmd, varargin)
   end
 
   if (strcmp(A{1}, 'COMMAND_ERROR_PYTHON'))
-
-    ex_popen2 = exist('popen2') > 1;
-    ipc_s = sympref('ipc'); 
-
-    if strcmp(ipc_s, 'popen2') || strcmp(ipc_s, 'sysoneline') || (strcmp(ipc_s, 'default') && ex_popen2)
-      errlineno = A{3} - LinesBeforeCmdBlock;
-    elseif strcmp(ipc_s, 'system') || strcmp(ipc_s, 'systmpfile') || (strcmp(ipc_s, 'default') && !ex_popen2)
-      errlineno = A{3} - db.prelines - LinesBeforeCmdBlock;
-    else
-      warning('IPC system not supported yet to handle the line error.');
-      error(A{2});
-    end
-
+    errlineno = A{3} - db.prelines - LinesBeforeCmdBlock;
     error(strcat(A{2}, ' (line: ', mat2str(errlineno), ')'));
   end
 
