64c824fd7b5f25a624c6468a2da2253b8f0d302a
Colin B. Macdonald <cbm@m.fsf.org>
2016-02-15 14:36:16 -0800

    Just one try: except: block for cmd
    Previously, we had a nested try: except: block.  Add some tests
    for strange blank lines: all seems safe when used inside
    a try:, even with popen2.
    
    Fixes #344.

--- inst/private/python_ipc_system.m
+++ inst/private/python_ipc_system.m
@@ -72,7 +72,7 @@ function [A, info] = python_ipc_system(what, cmd, mktmpfile, varargin)
   %% The actual command
   % cmd will be a snippet of python code that does something
   % with _ins and produce _outs.
-  s2 = python_format_cmd(cmd);
+  s2 = cmd;
 
   %% output, or perhaps a thrown error
   s3 = python_copy_vars_from('_outs');
