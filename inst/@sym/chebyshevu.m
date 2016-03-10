4c27ba2f65895dfae7aaed700a279ea840d9a902
Abhinav Tripathi <genuineabhinav@gmail.com>
2016-03-10 12:50:43 +0530

    changed to lowercase

--- inst/@sym/chebyshevu.m
+++ inst/@sym/chebyshevu.m
@@ -1,4 +1,4 @@
-﻿%% Copyright (C) 2016, Abhinav Tripathi
+%% Copyright (C) 2016, Abhinav Tripathi
 %%
 %% This file is part of OctSymPy.
 %%
@@ -18,27 +18,27 @@
 
 %% -*- texinfo -*-
 %% @documentencoding UTF-8
-%% @deftypefn  {Function File} {@var{s} =} chebyshevU (@var{x}, @var{n})
+%% @deftypefn  {Function File} {@var{s} =} chebyshevu (@var{x}, @var{n})
 %% Find the chebyshev's seconf type nth polynomial in the variable x.
 %%
 %% Example:
 %% @example
 %% @group
 %% >> syms x
-%% >> g = chebyshevU(x, 1)
+%% >> g = chebyshevu(x, 1)
 %%    @result{} g = 2x
-%% >> g = chebyshevU(x, 2)
+%% >> g = chebyshevu(x, 2)
 %%    @result{} g = 4x^2 - 1
 %% @end group
 %% @end example
 %%
-%% @seealso{chebyshevT}
+%% @seealso{chebyshevt}
 %% @end deftypefn
 
 %% Author: Abhinav Tripathi
 %% Keywords: symbolic
 
-function sympoly = chebyshevU(x, n)
+function sympoly = chebyshevu(x, n)
   cmd = { 'x, n = _ins'
           'if n == 0:'
           '    return sym(1);'
@@ -48,25 +48,26 @@ function sympoly = chebyshevU(x, n)
           'y1 = 2*x;'
           'for i in range(n):'
           '    y = 2*x*y1 - y0;'
+          '    y = y1;'
+          '    y1 = y;'
           'return y,' };
   sympoly = python_cmd (cmd, x, n);
-  sympoly
 end
 
 %!test
 %! syms x
 %! n0 = 0;
-%! poly0 = chebyshevU(x, n0);
+%! poly0 = chebyshevu(x, n0);
 %! assert(poly0 == sym(1))
 
 %!test
 %! syms x
 %! n1 = 1;
-%! poly1 = chebyshevU(x, n1);
+%! poly1 = chebyshevu(x, n1);
 %! assert(poly1 == (2*x))
 
 %!test
 %! syms x
 %! n2 = 2;
-%! poly2 = chebyshevU(x, n2);
+%! poly2 = chebyshevu(x, n2);
 %! assert(poly2 == (4*x*x - 1))
