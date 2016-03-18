5434f154adc42fb0eb37ea8674cad39aca1d5443
Colin B. Macdonald <cbm@m.fsf.org>
2016-03-18 20:33:07 -0300

    minor reformatting
    Use new texinfo style for docs, spaces with commas, remove tabs

--- inst/@sym/kron.m
+++ inst/@sym/kron.m
@@ -24,32 +24,31 @@
 %% Examples:
 %% @example
 %% @group
-%% >> syms x
-%% >> kron(eye(2)*x,[1,-1;-1,1])
-%% @result{} ans = (sym 4×4 matrix)
-%% 	
-%%     ⎡x   -x  0   0 ⎤
-%%     ⎢              ⎥
-%%     ⎢-x  x   0   0 ⎥
-%%     ⎢              ⎥
-%%     ⎢0   0   x   -x⎥
-%%     ⎢              ⎥
-%%     ⎣0   0   -x  x ⎦
-%%			
+%% syms x
+%% kron(eye(2)*x,[1,-1;-1,1])
+%%   @result{} ans = (sym 4×4 matrix)
+%%
+%%       ⎡x   -x  0   0 ⎤
+%%       ⎢              ⎥
+%%       ⎢-x  x   0   0 ⎥
+%%       ⎢              ⎥
+%%       ⎢0   0   x   -x⎥
+%%       ⎢              ⎥
+%%       ⎣0   0   -x  x ⎦
 %%
 %% @end group
 %% @end example
 %%
 %% @example
 %% @group
-%% >> syms x y
-%% >> kron([1,2],[x,y;y,x])
-%% @result{} ans = (sym 2×4 matrix)
+%% syms x y
+%% kron([1, 2], [x, y; y, x])
+%%   @result{} ans = (sym 2×4 matrix)
 %%
-%%     ⎡x  y  2⋅x  2⋅y⎤
-%%     ⎢              ⎥
-%%     ⎣y  x  2⋅y  2⋅x⎦
-%%						
+%%       ⎡x  y  2⋅x  2⋅y⎤
+%%       ⎢              ⎥
+%%       ⎣y  x  2⋅y  2⋅x⎦
+%%
 %% @end group
 %% @end example
 %% @end deftypefn
@@ -59,15 +58,15 @@
 
 function c = kron(a, b)
 
-	if (isscalar(a) || isscalar(b))
-		c=a*b;
-	else	
-  cmd = { 'a, b = _ins'
-  		    'from sympy.physics.quantum import TensorProduct'
-          'return TensorProduct(Matrix(a),Matrix(b)),'
-        };
-  c = python_cmd (cmd, sym(a), sym(b));
-	end
+  if (isscalar(a) || isscalar(b))
+    c=a*b;
+  else
+    cmd = { 'a, b = _ins'
+            'from sympy.physics.quantum import TensorProduct'
+            'return TensorProduct(Matrix(a), Matrix(b))'
+          };
+    c = python_cmd (cmd, sym(a), sym(b));
+  end
 end
 
 
@@ -76,41 +75,37 @@ end
 %! A = [sin(x), sin(y); x, y];
 %! B = ones(2);
 %! expected = sym([sin(x), sin(x), sin(y), sin(y); sin(x), sin(x), sin(y), sin(y); x, x, y, y; x, x, y, y]);
-%! assert (isequal(kron(A,B),expected))
-
+%! assert (isequal (kron(A, B), expected))
 
 %!test
 %! syms x y
 %! A = [sin(x), sin(y); x, y];
 %! B = 2;
-%! assert (isequal(kron(A,B),2*A))
-
+%! assert (isequal (kron(A, B), 2*A))
 
 %!test
 %! syms x y
 %! A = [sin(x), sin(y)];
 %! B = 2;
-%! assert (isequal(kron(B,A),2*A))
-
+%! assert (isequal( kron(B, A), 2*A))
 
 %!test
 %! syms x y;
 %! X = [tan(x), tan(x)];
 %! Y = [cot(x); cot(x)];
 %! expected = sym(ones(2));
-%! assert (isequal(simplify(kron(X,Y)), expected))
-
+%! assert (isequal (simplify(kron(X, Y)), expected))
 
 %!test
 %! syms x y z
 %! X = [x, y, z];
 %! Y = [y, y; x, x];
 %! expected = [x*y, x*y, y**2, y**2, y*z, y*z; x**2, x**2, x*y, x*y, x*z, x*z];
-%! assert (isequal(kron(X,Y), expected))
+%! assert (isequal (kron(X, Y), expected))
 
 %!test
 %! syms x y
 %! X = [x, x**2; y, y**2];
 %! Y = [1, 0; 0, 1];
 %! expected = [x, x**2, 0, 0; y, y**2, 0, 0; 0, 0, x, x**2; 0, 0, y, y**2];
-%! assert (isequal(kron(Y,X), expected))
+%! assert (isequal (kron(Y, X), expected))
