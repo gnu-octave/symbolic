da50ee286c4125326eecb5b29190cad923f037bc
Colin B. Macdonald <cbm@m.fsf.org>
2016-03-30 16:27:44 -0300

    white-space fixes

--- inst/@sym/sort.m
+++ inst/@sym/sort.m
@@ -4,10 +4,10 @@
 %%
 %% OctSymPy is free software; you can redistribute it and/or modify
 %% it under the terms of the GNU General Public License as published
-%% by the Free Software Foundation; either version 3 of the License, 
+%% by the Free Software Foundation; either version 3 of the License,
 %% or (at your option) any later version.
 %%
-%% This software is distributed in the hope that it will be useful, 
+%% This software is distributed in the hope that it will be useful,
 %% but WITHOUT ANY WARRANTY; without even the implied warranty
 %% of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
 %% the GNU General Public License for more details.
@@ -24,9 +24,9 @@
 %% @group
 %% sort([sym(2), sym(1)])
 %%   @result{} ans = (sym) [1  2]  (1×2 matrix)
-%%
 %% @end group
 %% @end example
+%%
 %% For matrices, sort orders the elements within columns
 %% @example
 %% @group
@@ -37,7 +37,6 @@
 %%               ⎢    ⎥
 %%               ⎣3  1⎦
 %%
-%%
 %% @end group
 %% @end example
 %% @end deftypefn
@@ -47,52 +46,52 @@
 
 function s = sort(f)
   if (rows(f) <=  1 && columns(f) <=  1)
-    s = f; 
+    s = f;
   else
-    cmd = { 
+    cmd = {
       '(f) = _ins'
       'f = Matrix(f).tolist()'
       's = []'
       'for c in f:'
       '    s.append(sorted(c))'
       'return Matrix(s)'
-      }; 
+      };
     if (rows(f)>1)
-      f = f'; 
-      s = python_cmd (cmd, f); 
-      s = s'; 
+      f = f';
+      s = python_cmd (cmd, f);
+      s = s';
     else
-      s = python_cmd (cmd, f); 
-    end     
-  end    
+      s = python_cmd (cmd, f);
+    end
+  end
 end
 
-%!test 
-%! f = [sym(1), sym(0)]; 
-%! expected = sym([0, 1]); 
-%! assert (isequal (sort(f), expected))
-
-%!test 
-%! f = [sym(1)]; 
-%! expected = sym(1); 
+%!test
+%! f = [sym(1), sym(0)];
+%! expected = sym([0, 1]);
 %! assert (isequal (sort(f), expected))
 
 %!test
-%! f = [sym(3), sym(2), sym(6)]; 
-%! s = sort(f); 
-%! expected_s = sym([2, 3, 6]); 
+%! f = [sym(1)];
+%! expected = sym(1);
+%! assert (isequal (sort(f), expected))
+
+%!test
+%! f = [sym(3), sym(2), sym(6)];
+%! s = sort(f);
+%! expected_s = sym([2, 3, 6]);
 %! assert (isequal (s, expected_s))
 
 %!test
-%! f = [sym(pi), sin(sym(2)), sqrt(sym(6))]; 
-%! s = sort(f); 
-%! expected_s = sym([sin(sym(2)), sqrt(sym(6)), sym(pi)]); 
+%! f = [sym(pi), sin(sym(2)), sqrt(sym(6))];
+%! s = sort(f);
+%! expected_s = sym([sin(sym(2)), sqrt(sym(6)), sym(pi)]);
 %! assert (isequal (s, expected_s))
 
-%!test 
-%! f = [sym(1), sym(2); sym(2), sym(pi); sym(pi), sym(1)]; 
-%! s = sort(f); 
-%! expected_s = ([sym(1), sym(1); sym(2), sym(2); sym(pi), sym(pi)]); 
+%!test
+%! f = [sym(1), sym(2); sym(2), sym(pi); sym(pi), sym(1)];
+%! s = sort(f);
+%! expected_s = ([sym(1), sym(1); sym(2), sym(2); sym(pi), sym(pi)]);
 %! assert (isequal (s, expected_s))
 
 %!test 
