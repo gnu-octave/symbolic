e66e05642d37f41725b5e9507e63c5943b62740e
utkarsh <utkarshgautam247@gmail.com>
2016-04-14 03:48:59 +0530

    added yn and jn

---
+++ inst/@sym/yn.m
@@ -0,0 +1,62 @@
+%% Copyright (C) 2016 Utkarsh Gautam 
+%%
+%% This file is part of OctSymPy.
+%%
+%% OctSymPy is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published
+%% by the Free Software Foundation; either version 3 of the License,
+%% or (at your option) any later version.
+%%
+%% This software is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty
+%% of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+%% the GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public
+%% License along with this software; see the file COPYING.
+%% If not, see <http://www.gnu.org/licenses/>.
+
+%% -*- texinfo -*-
+%% @documentencoding UTF-8
+%% @deftypefn  {Function File} {@var{Y} =} yn (@var{alpha}, @var{x})
+%% Symbolic Spherical Bessel function of the second kind.
+%%
+%% Example:
+%% @example
+%% @group
+%% syms n x
+%% A = 2*yn(n, x)
+%%   @result{} A = (sym) 2⋅yn(n, x)
+%% diff(A)
+%%   @result{} ans = (sym)
+%%   
+%%                      2⋅(n + 1)⋅yn(n, x)
+%%     2⋅yn(n - 1, x) - ──────────────────
+%%                           x   
+%% @end group
+%% @end example
+%%
+%% @seealso{besselj, besseli, besselk, jn}
+%% @end deftypefn
+
+function Y = yn(n, x)
+  if (nargin ~= 2)
+    print_usage ();
+  end
+  Y = binop_helper(n, x, 'yn');
+end
+
+
+
+%!test
+%! % roundtrip
+%! if (python_cmd ('return Version(spver) >= Version("1.0")'))
+%! syms x
+%! A = yn(2, 10);
+%! q = yn(2, x);
+%! h = function_handle(q);
+%! B = h(10);
+%! assert (abs (A - B) <= eps*abs(A))
+%! end
+
+%!error yn(sym('x'))
