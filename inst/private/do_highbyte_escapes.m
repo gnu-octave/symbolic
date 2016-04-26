ee7e087204d8a83d2853d6e96959ae8e4714497c
Colin B. Macdonald <cbm@m.fsf.org>
2016-04-26 13:13:40 -0700

    vectorized implementation

--- inst/private/do_highbyte_escapes.m
+++ inst/private/do_highbyte_escapes.m
@@ -9,21 +9,23 @@ function r = do_highbyte_escapes(s)
 %   >> do_highbyte_escapes(s)
 %      ans = a⌈y⌉b
 %
-%
-%   TODO: faster (vectorized) implementation is surely possible.
 
-  st = tic();
-  [S, E, TE, M, T, NM, SP] = regexp(s, '\\x(..)');
-  t1 = toc(st);
+  I = strfind(s, '\x');
+  % new string with X instead of escapes
+  r = regexprep(s, '\\x(..)',  'X');
 
-  st = tic;
-  i = 1;
-  r = '';
-  for j=1:length(S)
-    d = hex2dec(T{j}{1});
-    r = [r s(i:S(j)-1) char(d)];
-    i = E(j)+1;
+  % list of 2 digit hex numbers
+  s2 = s([I'+2 I'+3]);
+  % slowest part on Octave
+  dec = char(hex2dec(s2));
+
+  % positions of escaped bytes in the new string r
+  I = I - [0:4:(4*length(I)-1)] + [0:(length(I)-1)];
+  r(I) = dec;
+
+  if (~ exist ('octave_config_info', 'builtin'))
+    % matlab is not UTF-8 internally
+    r = native2unicode(uint8(r));
   end
-  r = [r s(i:end)];
-  t2 = toc(st);
-  disp(sprintf('time=[%g %g], r=%s', t1, t2, r))
+
+end
