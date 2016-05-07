a1a86a756f60b23e7080c16663a6026e38fcf4a6
Colin B. Macdonald <cbm@m.fsf.org>
2016-05-07 00:08:09 -0700

    add log gamma function and alias

---
+++ inst/@sym/lgamma.m
@@ -0,0 +1,35 @@
+%% Copyright (C) 2016 Colin B. Macdonald
+%%
+%% This file is part of OctSymPy.
+%%
+%% OctSymPy is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published
+%% by the Free Software Foundation; either version 3 of the License,
+%% or (at your option) any later version.
+%%
+%% This software is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty
+%% of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+%% the GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public
+%% License along with this software; see the file COPYING.
+%% If not, see <http://www.gnu.org/licenses/>.
+
+%% -*- texinfo -*-
+%% @documentencoding UTF-8
+%% @defmethod  @@sym gammaln (@var{x})
+%% @defmethodx @@sym lgamma (@var{x})
+%% Symbolic logarithm of the gamma function.
+%%
+%% @seealso{@@sym/gammaln}
+%% @end defmethod
+
+function y = lgamma(x)
+  y = gammaln(x);
+end
+
+
+%!test
+%! % tested by gammaln
+%! assert (isequal (lgamma (sym ('x')), gammaln (sym ('x'))))
