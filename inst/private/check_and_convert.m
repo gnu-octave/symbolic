a4558f27206cb034db1dd5446a5815a84214facb
Colin B. Macdonald <cbm@m.fsf.org>
2016-05-31 16:04:24 -0700

    reformat for spacing and whitespace, minor fixes

--- inst/private/check_and_convert.m
+++ inst/private/check_and_convert.m
@@ -1,45 +1,46 @@
 function obj = check_and_convert(var_name)
   newl = sprintf('\n');
-  
-  pyexec(strjoin({['if isinstance(', var_name, ', sp.Matrix) and ', var_name, '.shape == (1, 1):'],
-                  ['  ', var_name, ' = ', var_name, '[0, 0];']}, newl));
 
-  str_check_is_list = strjoin({['if isinstance(', var_name, ', (list, tuple)):'],
-                              '  is_list = True',
-                              'else:',
-                              '  is_list = False'},newl);
-  pyexec(str_check_is_list);
+  pyexec(strjoin({ ...
+     ['if isinstance(' var_name ', sp.Matrix) and ' var_name '.shape == (1, 1):'],
+     ['    ' var_name ' = ' var_name '[0, 0]']}, newl));
+
+  pyexec(strjoin({ ...
+     ['if isinstance(', var_name, ', (list, tuple)):'],
+      '    is_list = True',
+      'else:',
+      '    is_list = False'}, newl));
   is_list = pyeval('is_list');
-  
+
   if is_list
-    n = pyeval(strjoin({'len(', var_name, ')'}));
+    n = pyeval(['len(' var_name ')']);
   else
     n = 1;
   end
-  
+
   obj = {};
   for i=1:n
     if is_list
       cur_var = sprintf('%s[%d]', var_name, i-1);
     else
-      cur_var = sprintf(var_name);
+      cur_var = var_name;
     end
-    
+
     is_list_curvar = pyeval(['isinstance(', cur_var, ', (list, tuple))']);
     if is_list_curvar
-      obj(i) = {check_and_convert(cur_var)};
+      obj{i} = check_and_convert(cur_var);
     else
-      str_check_is_sym = strjoin({['if ', cur_var, ' is None or isinstance(', cur_var, ', (sp.Basic, sp.MatrixBase)):'],
-                                  '  is_sym = True',
-                                  'else:',
-                                  '  is_sym = False'},newl);
-      pyexec(str_check_is_sym);
+      pyexec(strjoin({ ...
+         ['if ' cur_var ' is None or isinstance(' cur_var ', (sp.Basic, sp.MatrixBase)):'],
+          '    is_sym = True',
+          'else:',
+          '    is_sym = False'}, newl));
       is_sym = pyeval('is_sym');
 
       if is_sym
-        obj(i) = {get_sym_from_python(cur_var)};
+        obj{i} = get_sym_from_python(cur_var);
       else
-        obj(i) = {pyeval(cur_var)};
+        obj{i} = pyeval(cur_var);
       end
     end
   end
