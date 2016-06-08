584cdb3895a4645cd60f82d250c41466e187dd01
Colin B. Macdonald <cbm@m.fsf.org>
2016-06-08 14:13:04 -0700

    Revert "workaround pytave issue where bools returned as doubles"
    This reverts commit 6d15ed35d87283e365c5bfad6f9d78a8e1f34533.

--- inst/@sym/isequaln.m
+++ inst/@sym/isequaln.m
@@ -58,9 +58,6 @@ function t = isequaln(x, y, varargin)
 
   t = python_cmd (cmd, sym(x), sym(y));
 
-  % workaround: https://bitbucket.org/mtmiller/pytave/issues/6
-  t = logical(t);
-
   if (~ islogical(t))
     error('nonboolean return from python');
   end
