0049bdff03076703d145b185b534a00b4350b625
Colin B. Macdonald <cbm@m.fsf.org>
2016-06-21 14:20:21 -0700

    Tests for escaped-strings were wrong
    The string we have in Octave should be passed into to Python without
    changing it.  This means we may have to do some escaping for the string
    to get through the IPC mechanism: with the current ones, this means
    calling "undo_string_escapes".  Invent a quick-n-dirty implementation
    of said for matlab.
    
    Fixes #487.

--- inst/private/python_copy_vars_to.m
+++ inst/private/python_copy_vars_to.m
@@ -56,6 +56,16 @@ function a = do_list(indent, in, varlist)
       c=c+1; a{c} = sprintf('%s%s.append(%s)', sp, in, sprintf(char(x)));
 
     elseif (ischar(x))
+      if (exist ('OCTAVE_VERSION', 'builtin'))
+        x = undo_string_escapes(x);
+      else
+        % roughly same as the above on Matlab?
+        x = strrep(x, '\', '\\');
+        x = strrep(x, '"', '\"');
+        for cc = {'\n' '\r' '\t' '\b' '\f'}
+          x = strrep(x, sprintf(cc{:}), cc{:});
+        end
+      end
       c=c+1; a{c} = [sp in '.append("' x '")'];
       % or do we want a printf() around the string?
       %c=c+1; a{c} = sprintf('%s%s.append("%s")', sp, in, x);
