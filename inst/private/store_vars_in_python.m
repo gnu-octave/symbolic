c8157db2549864969e9b6d4db43b2c4a4c030ed0
Abhinav Tripathi <genuinelucifer@gmail.com>
2016-06-24 15:46:33 +0530

    See if sym is stored.

--- inst/private/store_vars_in_python.m
+++ inst/private/store_vars_in_python.m
@@ -23,7 +23,9 @@ function store_vars_in_python (varname, L)
     x = L{i}
     if (isa(x, 'sym'));
       disp('debug: storing a sym'); fflush(stdout);
+	  pyexec(['print ' char(x)]);
       pyexec([varname '.append(' char(x) ')'])
+      disp('debug: storedd a sym'); fflush(stdout);
     elseif (iscell (x))
       disp('debug: dealing with a cell'); fflush(stdout);
       tempname = [varname num2str(counter)]
