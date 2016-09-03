32fadeb40d88b0c3f2f2a5decd9cb894ef9ea779
latot <felipematas@yahoo.com>
2016-09-03 18:54:07 -0300

    add dawson

---
+++ inst/@sym/dawson.m
@@ -0,0 +1,46 @@
+%% Copyright (C) 2016 Lagu
+%%
+%% This file is part of OctSymPy.
+%%
+%% OctSymPy is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published
+%% by the Free Software Foundation; either version 3 of the License,
+%% or (at your option) any later version.
+%%
+%% This software is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty
+%% of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+%% the GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public
+%% License along with this software; see the file COPYING.
+%% If not, see <http://www.gnu.org/licenses/>.
+
+%% -*- texinfo -*-
+%% @documentencoding UTF-8
+%% @defmethod @@sym cosd (@var{x})
+%% Symbolic cos function with input in degrees.
+%%
+%% Example:
+%% @example
+%% @group
+%% syms x
+%% dawson (x)
+%%   @result{} ans = (sym)
+%%        2        
+%%      -x         
+%%  √π⋅ℯ   ⋅erfi(x)
+%%  ───────────────
+%%         2     
+%% @end group
+%% @end example
+%% @seealso{@@sym/erfc, @@sym/erf, @@sym/erfcx, @@sym/erfi, @@sym/erfinv, @@sym/erfcinv}
+%% @end defmethod
+
+
+function y = dawson(x)
+  if (nargin ~= 1)
+    print_usage ();
+  end
+  y = uniop_helper (x, 'lambda a: exp(-a**2)*erfi(a)*(sqrt(S(pi))/2)');
+end
