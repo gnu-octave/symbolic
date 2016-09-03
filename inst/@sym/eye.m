851354b003bf65c95b94a8b3ba4b07b7aeba5ea6
latot <felipematas@yahoo.com>
2016-09-03 18:54:29 -0300

    add eye

---
+++ inst/@sym/eye.m
@@ -0,0 +1,76 @@
+%% Copyright (C) 2016 Lagu
+%%
+%% This file is part of OctSymPy.
+%%
+%% OctSymPy is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published
+%% by the Free Software Foundation; either version 3 of the License,
+%% or (at your option) any later version.
+%%
+%% This software is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty
+%% of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+%% the GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public
+%% License along with this software; see the file COPYING.
+%% If not, see <http://www.gnu.org/licenses/>.
+
+%% -*- texinfo -*-
+%% @documentencoding UTF-8
+%% @defmethod @@sym ones (@var{x})
+%% @defmethod @@sym ones (@var{x}, @var{y})
+%% @defmethod @@sym ones (@var{x}, @var{y}, @var{class})
+%% Return an identity matrix.
+%%
+%% Example:
+%% @example
+%% @group
+%% y = eye (sym(3))
+%%   @result{} ans = (sym 3×3 matrix)
+%%  ⎡1  0  0⎤
+%%  ⎢       ⎥
+%%  ⎢0  1  0⎥
+%%  ⎢       ⎥
+%%  ⎣0  0  1⎦
+%% @end group
+%% @end example
+%%
+%% @seealso{@@sym/zeros, @@sym/ones}
+%% @end defmethod
+
+%% Source: http://docs.sympy.org/dev/modules/matrices/matrices.html
+
+function y = eye(varargin)
+
+  if (nargin >= 2)
+    for i=1:size(varargin)(2)
+      if (ischar(varargin{i}))
+        y = sym(eye(cell2nosyms(varargin){:}));
+        return;
+      end
+    end
+  end
+
+  if (nargin > 1)
+    y = sym(eye(cell2nosyms(varargin){:}));
+    return;
+  end
+
+  y = python_cmd('return eye(*_ins),', varargin{:});
+end
+
+%!test
+%! y = eye(sym(2));
+%! x = [1 0;0 1];
+%! assert( isequal( y, sym(x)))
+
+%!test
+%! y = eye(sym(2), 1);
+%! x = [1;0];
+%! assert( isequal( y, sym(x)))
+
+%!test
+%! y = eye(sym(1), 2);
+%! x = [1 0];
+%! assert( isequal( y, sym(x)))
