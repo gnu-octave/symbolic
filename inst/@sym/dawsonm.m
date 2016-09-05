37fa05e53439b6528a419c49edeee7266d4cc3df
latot <felipematas@yahoo.com>
2016-09-05 10:41:13 -0300

    add dawsonm

---
+++ inst/@sym/dawsonm.m
@@ -0,0 +1,56 @@
+%% Copyright (C) 2016 Lagu
+%%
+%% This file is part of OctSymPy.
+%%
+%% OctSymPy is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published
+%% by the Free Software Foundation; either version 3 of the License,
+%% or (at your option) any later version.
+%%
+%% This software is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty
+%% of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+%% the GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public
+%% License along with this software; see the file COPYING.
+%% If not, see <http://www.gnu.org/licenses/>.
+
+%% -*- texinfo -*-
+%% @documentencoding UTF-8
+%% @defmethod @@sym dawsonm (@var{x})
+%% Compute the Dawson (scaled imaginary error) function in a matrix.
+%%
+%% Example:
+%% @example
+%% @group
+%% A = sym([1 2; 3 4]);
+%% dawsonm (A)
+%%   @result{} ans = (sym 2×2 matrix)
+%%  ⎡   ⎛ 7            10        ⎞      ⎛ 7            10        ⎞ ⎤
+%%  ⎢√π⋅⎝ℯ ⋅erfi(1) + ℯ  ⋅erfi(3)⎠   √π⋅⎝ℯ ⋅erfi(2) + ℯ  ⋅erfi(4)⎠ ⎥
+%%  ⎢─────────────────────────────   ───────────────────────────── ⎥
+%%  ⎢              2                               2               ⎥
+%%  ⎢                                                              ⎥
+%%  ⎢   ⎛ 15            22        ⎞     ⎛ 15            22        ⎞⎥
+%%  ⎢√π⋅⎝ℯ  ⋅erfi(1) + ℯ  ⋅erfi(3)⎠  √π⋅⎝ℯ  ⋅erfi(2) + ℯ  ⋅erfi(4)⎠⎥
+%%  ⎢──────────────────────────────  ──────────────────────────────⎥
+%%  ⎣              2                               2               ⎦
+%% @end group
+%% @end example
+%% @seealso{@@sym/dawson, @@sym/erfc, @@sym/erf, @@sym/erfcx, @@sym/erfi, @@sym/erfinv, @@sym/erfcinv}
+%% @end defmethod
+
+
+function y = dawsonm(x)
+  if (nargin ~= 1)
+    print_usage ();
+  end
+  y = exp(x^2)*erfi(x)*(sqrt(sym('pi'))/2);
+end
+
+%!test
+%! M = [0 1; 1 0];
+%! A = exp(M^2)*erfi(M)*(sqrt(pi)/2);
+%! B = double(dawsonm(sym(M)));
+%! assert(A, B, -eps*1.0062) %% Octave diverge in that proportion of error.
