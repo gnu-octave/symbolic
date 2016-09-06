6a72e97ff9b345f461bcf5d138150085f47d98a1
latot <felipematas@yahoo.com>
2016-09-06 16:42:33 -0300

    update sym2poly
    little fixes
    
    update things

--- inst/@sym/sym2poly.m
+++ inst/@sym/sym2poly.m
@@ -1,5 +1,6 @@
 %% Copyright (C) 2003 Willem J. Atsma
 %% Copyright (C) 2014-2016 Colin B. Macdonald
+%% Copyright (C) 2016 Lagu
 %%
 %% This program is free software; you can redistribute it and/or
 %% modify it under the terms of the GNU General Public
@@ -20,7 +21,7 @@
 %% @documentencoding UTF-8
 %% @deftypemethod  @@sym {@var{doublec} =} sym2poly (@var{p})
 %% @deftypemethodx @@sym {@var{c} =} sym2poly (@var{p}, @var{x})
-%% Return vector of coefficients of a symbolic polynomial.
+%% Return a double type vector of coefficients of a symbolic polynomial.
 %%
 %% In the two-input form, the second argument @var{x} specifies the free
 %% variable; in this case this function returns a row vector @var{c} of
@@ -30,29 +31,22 @@
 %% @group
 %% syms x y
 %% sym2poly(2*x^2 + 3*x - pi, x)
-%%    @result{} (sym) [2  3  -π]  (1×3 matrix)
+%%    @result{} ans =
+%%   2.0000   3.0000  -3.1416
 %% sym2poly(x^2 + y*x, x)
 %%    @result{} (sym) [1  y  0]  (1×3 matrix)
-%% @end group
-%% @end example
-%%
-%% @strong{Warning}: Using the single-argument form, the coefficient vector
-%% @var{c} is a plain numeric vector (double).  This is for compatibility
-%% with the Matlab Symbolic Math Toolbox, and could change in future versions
-%% of OctSymPy.  We thus recommend using @code{double} explicitly as in:
-%% @example
-%% @group
-%% syms x
-%% double(sym2poly(pi*x^2 + 3*x/2 + exp(sym(1))))
+%% sym2poly(pi*x^2 + 3*x/2 + exp(sym(1)))
 %%    @result{}     3.1416   1.5000   2.7183
 %% @end group
 %% @end example
+%%
+%% If you want a symbolic result check the sym2polysane function.
 %%
 %% If @var{p} is not a polynomial the result has no warranty.  SymPy can
 %% certainly deal with more general concepts of polynomial but we do not
 %% yet expose all of that here.
 %%
-%% @seealso{poly2sym, polyval, roots}
+%% @seealso{sym2polysane, poly2sym, polyval, roots}
 %% @end deftypemethod
 
 %% Created: 18 April 2003
@@ -66,47 +60,11 @@
 %%    Used the comment header and tests in OctSymPy, but rewrote
 %%    the body (by Colin Macdonald).
 
-function c = sym2poly(p, x)
+function c = sym2poly(varargin)
 
-  if ~(isscalar(p))
-    error('works for scalar input only');
-  end
+  c = sym2polysane(varargin{:});
 
-  if (nargin == 1)
-    ss = findsymbols(p);
-    if (length(ss) >= 2)
-      error('Input has more than one symbol: not clear what you want me to do')
-    end
-    x = ss{1};
-    convert_to_double = true;
-  elseif (nargin == 2)
-    convert_to_double = false;
-  else
-    print_usage ();
-  end
-
-  cmd = { 'f = _ins[0]'
-          'x = _ins[1]'
-          'p = Poly.from_expr(f,x)'
-          'return p.all_coeffs(),' };
-
-  c2 = python_cmd (cmd, sym(p), sym(x));
-  if (isempty(c2))
-    error('Empty python output, can this happen?  A bug.')
-  end
-
-  % FIXME: should be able to convert c2 to array faster than array
-  % expansion!  Particularly in the case where we just convert to
-  % double anyway!
-  c = sym([]);
-  for j = 1:numel(c2)
-    % Bug #17
-    %c(j) = c2{j};
-    idx.type = '()'; idx.subs = {j};
-    c = subsasgn(c, idx, c2{j});
-  end
-
-  if (convert_to_double)
+  if isempty(findsymbols(c))
     c = double(c);
   end
 
@@ -115,20 +73,19 @@ end
 
 %!shared x,y,a,b,c
 %! syms x y a b c
-%!assert (isequal (sym2poly (x^2 + 3*x - 4), [1 3 -4]))
-%!assert (isequal (sym2poly (x^6 - x^3), [1 0 0 -1 0 0 0]))
-%!assert (isequal (sym2poly (x^2 + 3*x - 4, x), [1 3 -4]))
-%!assert (norm (sym2poly (pi*x^2 + exp(sym(1))) - [pi 0 exp(1)]) < 10*eps)
+%! assert (isequal (sym2poly (x^2 + 3*x - 4), [1 3 -4]))
+%! assert (isequal (sym2poly (x^6 - x^3), [1 0 0 -1 0 0 0]))
+%! assert (isequal (sym2poly (x^2 + 3*x - 4, x), [1 3 -4]))
+%! assert (norm (sym2poly (pi*x^2 + exp(sym(1))) - [pi 0 exp(1)]) < 10*eps)
+%! assert (isa (sym2poly (x^2 + 3*x - 4), 'double'))
+%! assert (isequal (sym2poly (poly2sym ([1 2 3])), [1 2 3]))
 %% types
-%!assert (isa (sym2poly (x^2 + 3*x - 4), 'double'))
-%!assert (isa (sym2poly (x^2 + 3*x - 4, x), 'sym'))
 %% tests with other vars
-%!assert (isequal (sym2poly (x^2+y*x, x), [sym(1) y sym(0)]))
-%!assert (isequal (sym2poly (x^2+y*x, y), [x x^2]))
+%! assert (isequal (sym2poly (x^2+y*x, x), [sym(1) y sym(0)]))
+%! assert (isequal (sym2poly (x^2+y*x, y), [x x^2]))
 %% inverse relationship
-%!assert (isequal (sym2poly (poly2sym ([a b c], x), x), [a b c]))
-%!assert (isequal (poly2sym (sym2poly(a*x^2 + c, x), x), a*x^2 + c))
-%!assert (isequal (sym2poly (poly2sym ([1 2 3])), [1 2 3]))
+%! assert (isequal (sym2poly (poly2sym ([a b c], x), x), [a b c]))
+%! assert (isequal (poly2sym (sym2poly(a*x^2 + c, x), x), a*x^2 + c))
 
 %!error <more than one symbol>
 %! % too many symbols for single-input
