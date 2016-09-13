081606c2cbe4ee7ffbc2699c17af459614c91d6d
Colin B. Macdonald <cbm@m.fsf.org>
2016-09-12 17:09:16 -0700

    minor change to private fcn

--- inst/private/valid_sym_assumptions.m
+++ inst/private/valid_sym_assumptions.m
@@ -32,8 +32,8 @@ function L = valid_sym_assumptions()
 
   if (isempty(List))
 
-    cmd = {'from sympy.core.assumptions import _assume_defined as L'
-           'return list(L),'};
+    cmd = {'from sympy.core.assumptions import _assume_defined'
+           'return list(_assume_defined),'};
     List = python_cmd(cmd);
 
   end
