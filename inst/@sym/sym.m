200c70e48977c244561a82fc59a19bc369803e01
latot <felipematas@yahoo.com>
2016-11-05 17:06:56 -0300

    drop create_sym_with_s

--- inst/@sym/sym.m
+++ inst/@sym/sym.m
@@ -340,8 +340,37 @@ function s = sym(x, varargin)
         end
       end
 
-      s = create_sym_with_s (x);
+      cmd = {'x = "{s}"'
+             'try:'
+             '    return (0, (0, 0), S(x, rational=True))'
+             'except Exception as e:'
+             '    lis = set()'
+             '    if "(" in x or ")" in x:'
+             '        x2 = split("\(|\)| |,", x)'
+             '        x2 = [p for p in x2 if p]'
+             '        for i in x2:'
+             '            try:'
+             '                if eval("callable(" + i + ")"):'
+             '                    lis.add(i)'
+             '            except:'
+             '                pass'
+             '    if len(lis) > 0:'
+             '        return (str(e), (1, "" if len(lis) == 1 else "s"), "\", \"".join(str(e) for e in lis))'
+             '    return (str(e), (2, 0), x)' };
+
+      [err flag s] = python_cmd (strrep (cmd, '{s}', x));
+
+      switch (flag{1})
+        case 1  % Bad call to python function
+          disp (['Python: ' err]);
+          disp (['error: Error using the "' s '" Python function' flag{2} ', you wrote it correctly?']);
+          error ('if this do not was intentional please use other var name.');
+        case 2  % Something else
+          disp (['Python: ' err]);
+          error (['You can not use var name "' s '" for a error, if is a bug please report it.']);
+      end
       return
+
     end
   end
 
