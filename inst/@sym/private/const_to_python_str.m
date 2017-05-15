46153d955c312a43c49d99033d5722101f03f4a5
Colin B. Macdonald <cbm@m.fsf.org>
2017-05-15 15:48:16 -0700

    refactor private function
    It doesn't need to deal with doubles anymore

--- inst/@sym/private/const_to_python_str.m
+++ inst/@sym/private/const_to_python_str.m
@@ -28,30 +28,19 @@
 
 function [s, flag] = const_to_python_str (x)
 
-  % Table of special doubles and special strings.  Format for each row is:
-  %    double value, {list of strings to recognize}, resulting python expr
+  % Table of special strings.  Format for each row is:
+  %    <unused> {list of strings to recognize}, resulting python expr
   % Note: case sensitive
   % Note: python expr should be in list for identity "sym(sympy(x)) == x"
   list = {pi {'pi'} 'S.Pi'; ...
           inf {'inf' 'Inf' 'oo'} 'S.Infinity'; ...
           nan {'NaN' 'nan'} 'S.NaN'; ...
-          i {} 'S.ImaginaryUnit'};
-  % Special Sympy constants to recognize
-  const = {'zoo'};
+          [] {'zoo'} 'S.ComplexInfinity'};
 
   flag = 1;
 
-  if (isa (x, 'double'))  % Number comparison
-    for j = 1:length (list)
-      if (isequaln (x, list{j, 1}))
-        s = list{j, 3};
-        return
-      elseif (isequaln (x, -list{j, 1}))
-        s = ['-' list{j, 3}];
-        return
-      end
-    end
-  elseif (isa (x, 'char'))  % Char comparison
+  assert (ischar (x))
+
     for j = 1:length (list)
       for n = 1:length (list{j, 2})
         if (strcmp (x, list{j, 2}{n}) || strcmp (x, ['+' list{j, 2}{n}]))
@@ -63,31 +52,8 @@ function [s, flag] = const_to_python_str (x)
         end
       end
     end
-    for j = 1:length (const)   % Check if is a python constant
-      if (strcmp (x, const{j}) || strcmp (x, ['+' const{j}]))
-        s = const{j};
-        return
-      elseif (strcmp (x, ['-' const{j}]))
-        s = ['-' const{j}];
-        return
-      end
-    end
-  else
-    error ('Format not supported.')
-  end
 
-  if (isa (x, 'char'))
     flag = 0;
     s = x;
-    return
-  elseif (isa (x, 'double'))
-    if ((abs (x) < 1e15) && (mod (x,1) == 0))
-      % special treatment for "small" integers
-      s = num2str (x);  % better than sprintf('%d', large)
-    else
-      s = '';
-      flag = 0;
-    end
-  end
 
 end
