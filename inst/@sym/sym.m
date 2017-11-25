cf222296d49acbc70a37ea6d1bb2c8b0fa5be4d1
Colin B. Macdonald <cbm@m.fsf.org>
2017-11-25 00:50:07 -0800

    reformat docs for shorter lines

--- inst/@sym/sym.m
+++ inst/@sym/sym.m
@@ -90,7 +90,8 @@
 %% @example
 %% @group
 %% sym(0.1)
-%%   @print{} warning: passing floating-point values to sym is dangerous, see "help sym"
+%%   @print{} warning: passing floating-point values to sym is
+%%   @print{}          dangerous, see "help sym"
 %%   @result{} ans = (sym) 1/10
 %% @end group
 %% @end example
@@ -115,7 +116,8 @@
 %% @example
 %% @group
 %% y = sym(pi/100)
-%%   @print{} warning: passing floating-point values to sym is dangerous, see "help sym"
+%%   @print{} warning: passing floating-point values to sym is
+%%   @print{}          dangerous, see "help sym"
 %%   @result{} y = (sym)
 %%        π
 %%       ───
