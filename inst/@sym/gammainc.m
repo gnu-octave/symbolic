5d9056b3c1476ae5ea04f72f6d391d68c396ac07
Colin B. Macdonald <cbm@m.fsf.org>
2019-04-02 08:38:28 -0700

    gammainc: fix regularization
    gammainc is the regularized gamma function, so we need to divide by gamma
    when implementing in terms of SymPy's uppergamma and lowergamma.  Add
    tests.  Fixes #955.
    
    The other function `sym.igamma` remains unregularized: it was correct
    before this change.

--- inst/@sym/gammainc.m
+++ inst/@sym/gammainc.m
@@ -21,16 +21,25 @@
 %% @defmethod  @@sym gammainc (@var{x}, @var{a})
 %% @defmethodx @@sym gammainc (@var{x}, @var{a}, 'lower')
 %% @defmethodx @@sym gammainc (@var{x}, @var{a}, 'upper')
-%% Symbolic incomplete gamma function.
+%% Symbolic regularized incomplete gamma function.
 %%
 %% Example:
 %% @example
 %% @group
 %% syms x a
 %% gammainc(x, a)
-%%   @result{} (sym) γ(a, x)
-%% gammainc(x,a, 'upper')
-%%   @result{} (sym) Γ(a, x)
+%%   @result{} (sym)
+%%       γ(a, x)
+%%       ───────
+%%         Γ(a)
+%% @end group
+%%
+%% @group
+%% gammainc(x, a, 'upper')
+%%   @result{} (sym)
+%%       Γ(a, x)
+%%       ───────
+%%         Γ(a)
 %% @end group
 %% @end example
 %%
@@ -42,7 +51,10 @@
 %% gammainc(3, 1)
 %%   @result{} ans = 0.95021
 %% gammainc(x, a)
-%%   @result{} (sym) γ(a, x)
+%%   @result{} (sym)
+%%       γ(a, x)
+%%       ───────
+%%         Γ(a)
 %% double(subs(ans, [x a], [3 1]))
 %%   @result{} ans = 0.95021
 %% @end group
@@ -67,6 +79,7 @@ function y = gammainc(z, a, which)
   else
     print_usage ();
   end
+  y = y ./ gamma (a);
 end
 
 
@@ -86,6 +99,13 @@ end
 %!test
 %! % compare to double
 %! x = 5; a = 1;
+%! A = gammainc (x, a);
+%! B = double (gammainc (sym(x), a));
+%! assert(A, B, -eps)
+
+%!test
+%! % compare to double where gamma(a) != 1
+%! x = 5; a = 3;
 %! A = gammainc (x, a);
 %! B = double (gammainc (sym(x), a));
 %! assert(A, B, -eps)
@@ -162,9 +182,29 @@ end
 %! syms x a
 %! f = gammainc (x, a, 'upper');
 %! h = function_handle (f, 'vars', [x a]);
+%! A = h (1.1, 2);
+%! B = gammainc (1.1, 2, 'upper');
+%! assert (A, B)
+
+%!test
+%! % round trip
+%! syms x a
+%! f = gammainc (x, a, 'lower');
+%! h = function_handle (f, 'vars', [x a]);
+%! A = h (1.1, 2);
+%! B = gammainc (1.1, 2, 'lower');
+%! assert (A, B)
+
+%!test
+%! % round trip
+%! syms x a
+%! f = gammainc (x, a, 'upper');
+%! h = function_handle (f, 'vars', [x a]);
 %! A = h (1.1, 2.2);
 %! B = gammainc (1.1, 2.2, 'upper');
+%! if (python_cmd ('return Version(spver) > Version("1.3")'))
 %! assert (A, B)
+%! end
 
 %!test
 %! % round trip
@@ -173,4 +213,6 @@ end
 %! h = function_handle (f, 'vars', [x a]);
 %! A = h (1.1, 2.2);
 %! B = gammainc (1.1, 2.2, 'lower');
+%! if (python_cmd ('return Version(spver) > Version("1.3")'))
 %! assert (A, B)
+%! end
