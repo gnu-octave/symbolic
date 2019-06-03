de3d6331e3573f68f6e4c09708b00147ddb4f10d
Colin B. Macdonald <cbm@m.fsf.org>
2019-06-03 14:08:41 -0700

    remove references to pytave
    fix typos and refer to pythonic instead

--- inst/sympref.m
+++ inst/sympref.m
@@ -116,11 +116,10 @@
 %% Other options for @code{sympref ipc} include:
 %% @itemize
 %% @item @code{sympref ipc popen2}: force popen2 choice (e.g.,
-%% on Matlab were it would not be the default).
+%% on Matlab where it would not be the default).
 %% @item @code{sympref ipc native}: use native Python/C interface to
-%% interact directly with an embedded Python interpreter.
-%% This is highly experimental and requires functions provided by the
-%% ``pytave'' project which have not yet been merged into Octave.
+%% interact directly with an embedded Python interpreter, e.g.,
+%% provided by the Octave Pythonic package.
 %% @item @code{sympref ipc system}: construct a long string of
 %% the command and pass it directly to the python interpreter with
 %% the @code{system()} command.  This typically assembles a multiline
