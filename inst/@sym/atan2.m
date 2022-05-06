d4e0f502bfe1541b105e8356f9b9fb36efb4228d
Colin B. Macdonald <cbm@m.fsf.org>
2022-05-05 22:59:57 -0700

    atan2: error cases

--- inst/@sym/atan2.m
+++ inst/@sym/atan2.m
@@ -1,4 +1,4 @@
-%% Copyright (C) 2014, 2016, 2018 Colin B. Macdonald
+%% Copyright (C) 2014, 2016, 2018, 2022 Colin B. Macdonald
 %%
 %% This file is part of OctSymPy.
 %%
@@ -50,6 +50,9 @@ function a = atan2(y, x)
 
 end
 
+
+%!error atan2 (1)
+%!error atan2 (1, 2, 3)
 
 %!test
 %! % some angles
