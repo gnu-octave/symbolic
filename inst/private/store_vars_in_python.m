gnu-octave/symbolic

commit cd263b3853482226dbffe99486ff98c8445d6a63

private/store_vars_in_python: Fix handling of 0x0 matrix.
Previously, "pycall_sympy__ ('return _ins[0]', [])" evaluates to

--8<---------------cut here---------------start------------->8---
ans = [Python object of type array.array]

  array('d')
--8<---------------cut here---------------end--------------->8---

when using the pythonic interface, causing @sym/subsasgn test
failures.

Partly fixes #1067.

* inst/private/store_vars_in_python.m: Update it.
* inst/pycall_sympy__.m: Add test accordingly.

inst/private/store_vars_in_python.m
@@ -1,5 +1,6 @@
 %% Copyright (C) 2016 Abhinav Tripathi
 %% Copyright (C) 2016 Colin B. Macdonald
+%% Copyright (C) 2022 Alex Vong
 %%
 %% This file is part of OctSymPy.
 %%
@@ -25,6 +26,8 @@ function var_pyobj = store_vars_in_python (L)
       var_pyobj.append (pyeval (sympy (x)))
     elseif (iscell (x))
       var_pyobj.append (store_vars_in_python (x))
+    elseif (isequal (x, []))
+      var_pyobj.append (py.list ());
     else
       var_pyobj.append(x);
     end

inst/pycall_sympy__.m
@@ -1,4 +1,5 @@
 %% Copyright (C) 2014-2019, 2022 Colin B. Macdonald
+%% Copyright (C) 2022 Alex Vong
 %%
 %% This file is part of OctSymPy.
 %%
@@ -475,3 +476,8 @@ end
 %!test
 %! r = pycall_sympy__ ('return len(_ins[0])', '');
 %! assert (r == 0)
+
+%!test
+%! % ensure the 0x0 matrix in octave gets mapped to the empty list in python
+%! % @sym/subsasgn currently replies on this behaviour
+%! assert (pycall_sympy__ ('return _ins[0] == []', []));

commit aba48eb2cadefed8cc5e9134ff1339d59c08f627

private/store_vars_in_python: Fixes regression...
...caused by cd263b3853482226dbffe99486ff98c8445d6a63.

Closes #1151.

* inst/private/store_vars_in_python.m: Update it.
* inst/pycall_sympy__.m: Add test accordingly.

inst/private/store_vars_in_python.m
@@ -26,7 +26,7 @@ function var_pyobj = store_vars_in_python (L)
       var_pyobj.append (pyeval (sympy (x)))
     elseif (iscell (x))
       var_pyobj.append (store_vars_in_python (x))
-    elseif (isequal (x, []))
+    elseif (isnumeric (x) && isequal (x, [])) % See #1151 for more details
       var_pyobj.append (py.list ());
     else
       var_pyobj.append(x);

inst/pycall_sympy__.m
@@ -481,3 +481,7 @@ end
 %! % ensure the 0x0 matrix in octave gets mapped to the empty list in python
 %! % @sym/subsasgn currently replies on this behaviour
 %! assert (pycall_sympy__ ('return _ins[0] == []', []));
+
+%!test
+%! % ensure the empty string gets mapped to the empty string in python
+%! assert (pycall_sympy__ ('return _ins[0] == ""', ''));